23a65cb1be75446ab4c5d9cd201c10713b46c056
Seth NeJame <sethnejame@gmail.com>
2019-07-03 08:56:38 +0200

    Adding random user throw button functionality

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
 console.log("It's Rock, Paper, Scissors time!");
+
+let userThrowButton = document.getElementById("user-throw")
+
+userThrowButton.addEventListener("click", () => {
+
+    let rpsArr = ['Awe snap, son!  You threw Rock!', 'Everybody hit the deck!  You threw Paper!', 'Cover your eyes!  You threw Scissors!']
+    console.log(rpsArr) 
+
+    let userThrowRandomizer = rpsArr[Math.floor(Math.random()*rpsArr.length)]
+    console.log(userThrowRandomizer)
+
+    let showThrowDiv = document.getElementById("result")
+    console.log(showThrowDiv)
+
+    showThrowDiv.innerHTML = (userThrowRandomizer)
+
+
+}) 
